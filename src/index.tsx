6e5f4c73a3af25c2dc49f4f6d8235ca7ffb8102b
wongandrew1 <165810085+wongandrew1@users.noreply.github.com>
2025-04-30 01:46:44 -0400

    fix popup rendering

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = document.createElement("div")
-root.className = "container"
-document.body.appendChild(root)
-const rootDiv = ReactDOM.createRoot(root);
-rootDiv.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("No root element found");
+}
